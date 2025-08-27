698688392d9c919006e8b059f71621abaca9bd25
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2025-08-28 04:26:38 +0600

    DOM Content Loaded Event

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,11 @@
 /* S M SAMIUL HASAN */
 // main.js - Emergency Services Directory Functionality
+
+// DOM Content Loaded Event
+document.addEventListener('DOMContentLoaded' ,
+function() {
+
+
+
+
+});
